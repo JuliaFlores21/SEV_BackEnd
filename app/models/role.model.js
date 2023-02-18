2a62408a9d16193d1ac64b58ede38baa00661642
JuliaFlores21 <julia.flores@eagles.oc.edu>
2023-02-17 19:21:51 -0600

    feat (model): add faculty bio

--- app/models/role.model.js
+++ app/models/role.model.js
@@ -17,6 +17,10 @@ module.exports = (sequelize, Sequelize) =>
       {
         type: Sequelize.STRING
       },
+      facultyBio:
+      {
+        type: Sequelize.STRING
+      },
       accompanistType: 
       {
         type: Sequelize.STRING
