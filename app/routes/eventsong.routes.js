ba43343282be532b799fadf2e5351c5b3003c4f2
cjackson027 <c.jackson@eagles.oc.edu>
2023-03-02 12:50:07 -0600

    feat(routes): added event song route

---
+++ app/routes/eventsong.routes.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+    const critique = require("../controllers/eventsong.controller.js");
+    const { authenticate } = require("../authorization/authorization.js");
+    var router = require("express").Router();
+  
+    // Create a new eventsong
+    router.post("/", [authenticate], eventsong.create);
+  
+    // Retrieve all eventsong
+    router.get("/", [authenticate], eventsong.findAll);
+  
+    // Retrieve a single eventsong with id
+    router.get("/:id", [authenticate], eventsong.findOne);
+  
+    // Update a eventsong with id
+    router.put("/:id", [authenticate], eventsong.update);
+  
+    // Delete a eventsong with id
+    router.delete("/:id", [authenticate], eventsong.delete);
+  
+    // Delete all eventsong
+    // router.delete("/", [authenticate], eventsong.deleteAll);
+  
+    app.use('/eventsong', router);
+  };
